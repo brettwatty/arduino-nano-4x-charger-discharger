cb5d94dcd70ac31fba86d8be7f955f2532be9eb3
brettwatty <Brett.Watt@niwa.co.nz>
2019-09-02 10:06:46 +1200

    Fixed

--- test-sketches/ASCD_Nano_Get_DS18B20_Serials/ASCD_Nano_Get_DS18B20_Serials.ino
+++ test-sketches/ASCD_Nano_Get_DS18B20_Serials/ASCD_Nano_Get_DS18B20_Serials.ino
@@ -1,7 +1,7 @@
 // ASDC Nano 4x Arduino Charger / Discharger
 // ---------------------------------------------------------------------------
 // Created by Brett Watt on 19/03/2019
-// Copyright 2018 - Under creative commons license 3.0:
+// Copyright 2019 - Under creative commons license 3.0:
 //
 // This software is furnished "as is", without technical support, and with no
 // warranty, express or implied, as to its usefulness for any purpose.
@@ -24,18 +24,18 @@
 // Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
 OneWire oneWire(ONE_WIRE_BUS);
 
-// Pass our oneWire reference to Dallas Temperature. 
+// Pass our oneWire reference to Dallas Temperature.
 DallasTemperature sensors(&oneWire);
 
 // arrays to hold device addresses
-DeviceAddress tempSensorSerial[9];
+DeviceAddress tempSensorSerial[5];
 
-byte deviceCount = 5; // 5 in the ASDC Nano 4x Arduino Charger / Discharger
+byte deviceCount = 5; // 9 in the ASDC Mega 8x Arduino Charger / Discharger
 float sensorTempAverage = 0;
 bool tempSensorSerialCompleted[10];
 bool detectionComplete = false;
 byte tempSensorSerialOutput[5]; //Sensors 1 - 5
-byte pendingDetection = 0; // This will be from Battery 1 to 8 and then 9 for the ambient temperature
+byte pendingDetection = 0;      // This will be from Battery 1 to 4 and then 5 for the ambient temperature
 
 void setup(void)
 {
@@ -56,51 +56,185 @@ void setup(void)
   {
     Serial.println("");
     Serial.print("ERROR did no detect all the Sensors ");
-    Serial.print(sensors.getDeviceCount(), DEC);  
+    Serial.print(sensors.getDeviceCount(), DEC);
     Serial.print(" found. ");
     Serial.print(deviceCount);
     Serial.println(" Sensors should exist.");
-    while(1);
+
+    Serial.println("");
+    Serial.println("Atempting to use generic One Wire search to find non genuine Dallas DS18B20 sensors......");
+    Serial.print("Found ");
+    byte foundCount = useOneWireSearch();
+    Serial.print(foundCount);
+    Serial.println(" devices using One Wire Search.");
+    if (foundCount < deviceCount)
+    {
+      Serial.println("");
+      Serial.print("ERROR did no detect all the Sensors using One Wire Search ");
+      Serial.print(foundCount);
+      Serial.print(" found. ");
+      Serial.print(deviceCount);
+      Serial.println(" Sensors should exist.");
+      while (1)
+        ;
+    }
   }
 
-  oneWire.reset_search();
+  // oneWire.reset_search();
 
-  for (uint8_t i = 0; i < deviceCount; i++)
+  byte i = 0;
+  byte address[8];
+  if (oneWire.search(tempSensorSerial[i]))
   {
-    if (!oneWire.search(tempSensorSerial[i])) 
+    do
     {
-      Serial.print("Unable to find address for ");
-      Serial.println(i);
-    }
-    Serial.print("Device ");
-    Serial.print(i);
-    Serial.print(" Address: "); 
-    printAddress(tempSensorSerial[i]);
-    Serial.println();
-    sensors.setResolution(tempSensorSerial[i], TEMPERATURE_PRECISION);
+      delay(2000);
+      Serial.print("Device ");
+      Serial.print(i);
+      Serial.print(" Address: ");
+      printAddress(tempSensorSerial[i], false, false, false);
+      Serial.println();
+      sensors.setResolution(tempSensorSerial[i], TEMPERATURE_PRECISION);
+      i++;
+    } while (oneWire.search(tempSensorSerial[i]));
   }
 
   sensors.requestTemperatures();
-  for (uint8_t i = 0; i < deviceCount; i++)
+  for (byte i = 0; i < deviceCount; i++)
   {
     sensorTempAverage += sensors.getTempC(tempSensorSerial[i]);
   }
   sensorTempAverage = sensorTempAverage / deviceCount;
+}
 
+void loop(void)
+{
+  if (detectionComplete == false)
+  {
+    //Serial.print("Average Temp: ");
+    //Serial.println(sensorTempAverage);
+    Serial.print("-------------------------------------");
+    Serial.print("Heat Up Battery Sensor: ");
+    Serial.print(pendingDetection + 1);
+    Serial.println("-------------------------------------");
+    sensors.requestTemperatures();
+    for (byte i = 0; i < deviceCount; i++)
+    {
+      if (tempSensorSerialCompleted[i] == false)
+      {
+        if (pendingDetection != (deviceCount - 1))
+        {
+          Serial.print("Sensor ");
+          Serial.print(i);
+          Serial.print(" Temp: ");
+          Serial.println(sensors.getTempC(tempSensorSerial[i]));
+          if (sensors.getTempC(tempSensorSerial[i]) > (2.5 + sensorTempAverage))
+          {
+            Serial.print("Detected Battery: ");
+            Serial.println(pendingDetection + 1);
+            tempSensorSerialCompleted[i] = true;
+            tempSensorSerialOutput[pendingDetection] = i;
+            pendingDetection++; //If not greater than number of devices - last one = ambiant
+          }
+        }
+        else
+        {
+          Serial.println("");
+          Serial.println("");
+          Serial.println("");
+          Serial.println("");
+          Serial.print("-------------------------------------");
+          Serial.print("Detected Ambient Sensor Completed");
+          Serial.println("-------------------------------------");
+          tempSensorSerialCompleted[i] = true;
+          // Got the last one we are done
+          tempSensorSerialOutput[pendingDetection] = i;
+          detectionComplete = true;
+        }
+      }
+    }
+    delay(4000);
+  }
+  else
+  {
+    //else show serials
+    Serial.println("");
+    Serial.println("");
+    Serial.println("");
+    Serial.println("");
+    Serial.println("-------------------------------------");
+    Serial.println("Copy and Paste these Addresses into the Arduino Charger / Discharger Sketch");
+    Serial.println("-------------------------------------");
+    for (byte i = 0; i < deviceCount; i++)
+    {
+      if (i == 0)
+      {
+        printAddress(tempSensorSerial[tempSensorSerialOutput[i]], true, false, true);
+      }
+      else if (i == (deviceCount - 1))
+      {
+        printAddress(tempSensorSerial[tempSensorSerialOutput[i]], false, true, false);
+      }
+      else
+      {
+        printAddress(tempSensorSerial[tempSensorSerialOutput[i]], false, false, true);
+      }
+    }
+    Serial.println("-------------------------------------");
+    while (1)
+      ;
+  }
 }
 
 // Function to print a device address
-void printAddress(DeviceAddress deviceAddress)
+void printAddress(DeviceAddress deviceAddress, bool first, bool last, bool comma)
 {
-  Serial.print("{");
-  for (uint8_t i = 0; i < 8; i++)
+  if (first)
+  {
+    Serial.println("DeviceAddress tempSensorSerial[5]= {{");
+  }
+  else
+  {
+    Serial.print("{");
+  }
+  for (byte i = 0; i < 8; i++)
   {
     Serial.print("0x");
-    if (deviceAddress[i] < 0x10) Serial.print("0");
+    if (deviceAddress[i] < 0x10)
+      Serial.print("0");
     Serial.print(deviceAddress[i], HEX);
-    if (i < 7) Serial.print(", ");
+    if (i < 7)
+      Serial.print(", ");
   }
-  Serial.println("},"); 
+  if (last)
+  {
+    Serial.println("}};");
+  }
+  else
+  {
+    if (comma)
+    {
+      Serial.println("},");
+    }
+    else
+    {
+      Serial.print("}");
+    }
+  }
+}
+
+byte useOneWireSearch()
+{
+  byte i = 0;
+  byte address[8];
+  if (oneWire.search(address))
+  {
+    do
+    {
+      i++;
+    } while (oneWire.search(address));
+  }
+  return i;
 }
 
 // Function to print the temperature for a device
@@ -118,80 +252,5 @@ void printResolution(DeviceAddress deviceAddress)
 {
   Serial.print("Resolution: ");
   Serial.print(sensors.getResolution(deviceAddress));
-  Serial.println();    
-}
-
-// Main function to print information about a device
-void printData(DeviceAddress deviceAddress)
-{
-  Serial.print("Device Address: ");
-  printAddress(deviceAddress);
-  Serial.print(" ");
-  printTemperature(deviceAddress);
   Serial.println();
-}
-
-
-void loop(void)
-{ 
-  if(detectionComplete == false)
-  {
-    //Serial.print("Average Temp: ");
-    //Serial.println(sensorTempAverage);
-    Serial.print("-------------------------------------");  
-    Serial.print("Heat Up Battery Sensor: ");
-    Serial.print(pendingDetection + 1); 
-    Serial.println("-------------------------------------");  
-    sensors.requestTemperatures();
-    for (uint8_t i = 0; i < deviceCount; i++)
-    {
-      if(tempSensorSerialCompleted[i] == false)
-      {
-        if (pendingDetection != (deviceCount - 1)) 
-        {
-          Serial.print("Sensor ");
-          Serial.print(i);
-          Serial.print(" Temp: ");
-          Serial.println(sensors.getTempC(tempSensorSerial[i]));      
-          if(sensors.getTempC(tempSensorSerial[i]) > (2.5 + sensorTempAverage))
-          {
-            Serial.print("Detected Battery: ");
-            Serial.println(pendingDetection + 1);
-            tempSensorSerialCompleted[i] = true;
-            tempSensorSerialOutput[pendingDetection] = i;
-            pendingDetection++; //If not greater than number of devices - last one = ambiant
-          }
-        } else {
-          Serial.println("");
-          Serial.println("");
-          Serial.println("");
-          Serial.println("");
-          Serial.print("-------------------------------------");  
-          Serial.print("Detected Ambient Sensor Completed");
-          Serial.println("-------------------------------------");  
-          tempSensorSerialCompleted[i] = true;
-          // Got the last one we are done
-          tempSensorSerialOutput[pendingDetection] = i;
-          detectionComplete = true;
-        }
-      }
-    }
-    delay(4000);
-  } else {
-    //else show serials
-    Serial.println("");
-    Serial.println("");
-    Serial.println("");
-    Serial.println("");
-    Serial.println("-------------------------------------");  
-    Serial.println("Copy and Paste these Addresses into the Arduino Charger / Discharger Sketch");
-    Serial.println("-------------------------------------");  
-    for (uint8_t i = 0; i < deviceCount; i++)
-    {
-      printAddress(tempSensorSerial[tempSensorSerialOutput[i]]);
-    }
-    Serial.println("-------------------------------------");  
-    while(1);
-    
-  }
 }
