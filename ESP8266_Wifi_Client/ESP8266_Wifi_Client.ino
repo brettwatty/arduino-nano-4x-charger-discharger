c738c07d682fbae55e8d982f7865a589d70038db
brettwatty <41553730+brettwatty@users.noreply.github.com>
2020-09-17 08:10:59 +1200

    Update ESP8266_Wifi_Client.ino

--- ESP8266_Wifi_Client/ESP8266_Wifi_Client.ino
+++ ESP8266_Wifi_Client/ESP8266_Wifi_Client.ino
@@ -21,7 +21,7 @@ const char ssid[] = "";                        // SSID
 const char password[] = "";                    // Password
 const char server[] = "submit.vortexit.co.nz"; // Server to connect to send and recieve data
 const char userHash[] = "";                    // Database Hash - this is unique per user - Get this from Charger / Discharger Menu -> View
-const byte CDUnitID = 0;                       // CDUnitID this is the Units ID - this is unique per user - Get this from Charger / Discharger Menu -> View -> Select your Charger / Discharger
+const int CDUnitID = 0;                       // CDUnitID this is the Units ID - this is unique per user - Get this from Charger / Discharger Menu -> View -> Select your Charger / Discharger
 
 // readPage Variables
 char serverResult[32];  // String for incoming serial data
